938c02cd58b2564019721772ec8f270a9e329e40
xiaocao <853044263@qq.com>
2022-03-11 19:28:23 +0800

    deploy

---
+++ precache-manifest.275af31689de235febd5121ba72b66cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4890deb17ee8bcfff2",
+    "url": "/deom2-wangcai/css/app.2fb47c5d.css"
+  },
+  {
+    "revision": "3add7dc712ae55db2ad6bcabb7239aa9",
+    "url": "/deom2-wangcai/index.html"
+  },
+  {
+    "revision": "6b4890deb17ee8bcfff2",
+    "url": "/deom2-wangcai/js/app.970dcc63.js"
+  },
+  {
+    "revision": "f49ef87304d988f4b121",
+    "url": "/deom2-wangcai/js/chunk-vendors.64e5fb48.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/deom2-wangcai/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/deom2-wangcai/robots.txt"
+  }
+]);
